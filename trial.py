71fb3c28512f657df63cb155ca30d0a2623c66e0
devil-shiva <shivamsaksham2000@gmail.com>
2021-09-15 11:28:44 +0530

    added pm permit

--- trial.py
+++ trial.py
@@ -1,10 +1,15 @@
 import logging
 import os
+from time import sleep
 from PIL import Image 
-from telethon import TelegramClient , events
+from telethon import TelegramClient , events , utils
+from telethon.tl import functions
 
 from telethon.tl.functions.photos import UploadProfilePhotoRequest
 from datetime import datetime
+
+from telethon.tl.types import PeerUser, User
+
 
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
@@ -15,6 +20,31 @@ api_hash = 'bf24ab6d9b49c3855f161f9d5aec707c'
 
 client = TelegramClient('devil', api_id , api_hash)
 
+is_pm_verified = []
+is_pm_disapproved= []
+
+@client.on(events.NewMessage(outgoing=True , pattern=r'\.ap' ))
+async def approvePm(event):
+    chat = await event.get_chat()
+    is_pm_verified.append(chat.id)
+    await client.edit_message(event.message , "User Verified..")
+    sleep(2)
+    await event.message.delete()
+
+# <---------------------- approve ------------------->
+@client.on(events.NewMessage(incoming=True , func=lambda e: e.is_private))
+async def pmPermit(event):
+    chat = await event.get_chat()
+    msg_krne_wala = chat.username
+    me = await client.get_me()
+    if (chat.id in is_pm_verified):
+        return
+    else:
+        photu = await client.download_profile_photo(me.username)
+        await client.send_message(chat , "Hey!! I am Gabbar UserBot of My Master [Dracula](@{})\n\nMy Master is a Very busy Person. If you are here for timepass then please FO. \n\nIf want some Help, Join [Here](https://t.me/team_HackTwist). My master will try to see your PM whenever he gets Time.\n\nThank You!!".format(me.username) , file=photu , link_preview=False)
+        await client(functions.contacts.BlockRequest(id=msg_krne_wala))
+        os.remove(photu)
+        
 # <---------------------- ping ------------------->
 @client.on(events.NewMessage(outgoing=True , pattern=r'\.ping'))
 async def pingHandler(event):
@@ -25,15 +55,9 @@ async def pingHandler(event):
     await msg.edit(f"**Pong!!**\n `{ms} ms`")
 
 # <---------------------- pm hello ------------------->
-@client.on(events.NewMessage(incoming=True , pattern='(?i)hello'))
+@client.on(events.NewMessage( func=lambda e: e.is_private,incoming=True , pattern='(?i)hello'))
 async def greeting(event):
-    chat = await event.get_chat()
-    try:
-        is_channel = chat.title
-        pass
-    except:
-        
-        await event.reply('hi!!! How are you?')
+    await event.reply('hi!!! How are you?')
         
     
 # <---------------------- Get Pfp ------------------->
@@ -49,6 +73,24 @@ async def handler(event):
             await client.upload_file(sender_profile_pic)
         ))
         await replied.reply("Phomtu chumra Liya. ab Kya krega \n \n Stole steals stolen your Photu ")
+
+# <---------------------- Save to saved msg ------------------->
+@client.on(events.NewMessage(outgoing=True , pattern=r'\.save'))
+async def saveHandler(event):
+    if event.is_reply:
+        replied = await event.get_reply_message()
+        is_tag = event.message.raw_text.split()
+        try:
+            tag = is_tag[1]
+            await client.send_message('me' , "#{}".format(tag))
+        except:
+            await client.edit_message(event.message , "No Tag..")
+            sleep(1)
+        me = await client.get_me()
+        await client.forward_messages('me' , replied)
+        await client.edit_message(event.message , "Saved To your Cloud [Here](https://t.me/{})".format(me.username) , link_preview=False)
+        sleep(3)
+        await event.message.delete()
 
 # <---------------------- reverse text ------------------->
 @client.on(events.NewMessage(outgoing=True , pattern=r'\.ulta'))
@@ -104,8 +146,6 @@ async def reverseHandler(event):
         os.remove(image)
     else:
         await client.edit_message(event.message , "Reply to any Image")
-    
-        
 
 # <---------------------- about me ------------------->
 @client.on(events.NewMessage(outgoing=True , pattern=r'\.me'))
