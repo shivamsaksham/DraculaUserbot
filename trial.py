61c7c5a68d6d4cc73fc5eb9be8c21888eba6fa9a
devil-shiva <shivamsaksham2000@gmail.com>
2021-09-16 10:23:06 +0530

    app.json added

--- trial.py
+++ trial.py
@@ -1,7 +1,7 @@
 
 import logging
 from telethon import TelegramClient
-import handlers.news , handlers.alive , handlers.stoi , handlers.itos , handlers.iam , handlers.quot , handlers.reverse , handlers.save , handlers.photochor , handlers.greetings , handlers.ping , handlers.pmpermit , handlers.approvepm , handlers.meme , handlers.help
+import handlers.news , handlers.alive , handlers.stoi , handlers.itos , handlers.iam , handlers.quot , handlers.reverse , handlers.save , handlers.photochor , handlers.greetings , handlers.ping , handlers.pmpermit , handlers.approvepm , handlers.meme , handlers.help , handlers.client
 
 
 
@@ -9,15 +9,7 @@ import handlers.news , handlers.alive , handlers.stoi , handlers.itos , handlers
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.WARNING)
 
-api_id = 1433221
-api_hash = 'bf24ab6d9b49c3855f161f9d5aec707c'
-
-client = TelegramClient('devil', api_id , api_hash)
-
-
-
-
-
+client = handlers.client.client
 
 # <---------------------- help ------------------->
 with client as dracula:
