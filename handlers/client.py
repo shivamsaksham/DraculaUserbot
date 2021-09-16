153c69fcfb23fa221291a4fe4e59a293eb0db87c
sanskari-baalak <90844040+sanskari-baalak@users.noreply.github.com>
2021-09-16 19:18:57 +0530

    Update client.py

--- handlers/client.py
+++ handlers/client.py
@@ -6,4 +6,15 @@ string = os.environ['STRING_SESSION']
 api_id = os.environ['API_KEY']
 api_hash = os.environ['API_HASH']
 news_api = os.environ['NEWS_API']
-client = TelegramClient(StringSession(string), api_id , api_hash)
+
+with TelegramClient(StringSession(string), api_id, api_hash) as client:
+    ...  # use the client
+
+    
+    string = client.session.save()
+
+# Note that it's also possible to save any other session type
+# as a string by using ``StringSession.save(session_instance)``:
+# client = TelegramClient('sqlite-session', api_id, api_hash)
+# string = StringSession.save(client.session)
+# client = TelegramClient(StringSession(string), api_id , api_hash)
