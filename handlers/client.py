fbf08df4ce7fc3ec9b69037f8029a34d05b75354
devil-shiva <shivamsaksham2000@gmail.com>
2021-09-16 12:03:30 +0530

    os

--- handlers/client.py
+++ handlers/client.py
@@ -1,6 +1,7 @@
 from telethon import TelegramClient 
+import os
 
-api_id = 1433221
-api_hash = 'bf24ab6d9b49c3855f161f9d5aec707c'
-news_api = 'e97893d1464649e5813ff9b3f5b7e412'
+api_id = os.environ['API_KEY']
+api_hash = os.environ['API_HASH']
+news_api = os.environ['NEWS_API']
 client = TelegramClient('devil', api_id , api_hash)
