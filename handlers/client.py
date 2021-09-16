3da9f510e651bb7572cd3eb2a5a6f6f207a50cf9
sanskari-baalak <90844040+sanskari-baalak@users.noreply.github.com>
2021-09-16 19:40:40 +0530

    Update client.py

--- handlers/client.py
+++ handlers/client.py
@@ -1,5 +1,5 @@
 from telethon import TelegramClient 
-from telethon.sessions import StringSession
+
 import os
 
 string = os.environ['STRING_SESSION']
@@ -7,9 +7,7 @@ api_id = os.environ['API_KEY']
 api_hash = os.environ['API_HASH']
 news_api = os.environ['NEWS_API']
 
-with TelegramClient(StringSession(string), api_id, api_hash) as client:
-    
-    client.loop.run_until_complete()
+# with TelegramClient(StringSession(), api_id, api_hash) as client:
+#     session_str
 
-
-# client = TelegramClient(StringSession(string), api_id , api_hash)
+client = TelegramClient(string, api_id , api_hash)
