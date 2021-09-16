e72a32cfd15921844cda762ffdc3e71f362a7b58
sanskari-baalak <90844040+sanskari-baalak@users.noreply.github.com>
2021-09-16 19:13:25 +0530

    Added string session

--- handlers/client.py
+++ handlers/client.py
@@ -1,7 +1,9 @@
 from telethon import TelegramClient 
+from telethon.sessions import StringSession
 import os
 
+string = os.environ['STRING_SESSION']
 api_id = os.environ['API_KEY']
 api_hash = os.environ['API_HASH']
 news_api = os.environ['NEWS_API']
-client = TelegramClient('devil', api_id , api_hash)
+client = TelegramClient(StringSession(string), api_id , api_hash)
